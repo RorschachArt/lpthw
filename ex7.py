5caf41430413e36290f96fdab47ccb1b53e62233
RorschachArt <96551787+RorschachArt@users.noreply.github.com>
2022-01-03 17:00:17 -0600

    commit

---
+++ ex7.py
@@ -0,0 +1,19 @@
+# assign 10 to types_of_people
+types_of_people =10
+# assign the f-string to x (insert types_of_people in the string)
+x = f"There are {types_of_people} types of people."
+
+binary = "binary"
+do_not = "don't"
+y = f"Those who know {binary} and those who {do_not}."
+
+print(x)
+print(y)
+
+print(f"I said: {x}")
+print(f"I also said")
+
+Hilarious = False
+joke_evaluation = "Isn't that joke so funny?! {}"
+
+print(joke_evaluation.format(Hilarious))
