fe5d8597e7434d60d7e368c9f26f9c54247c1330
RorschachArt <96551787+RorschachArt@users.noreply.github.com>
2022-01-05 14:30:16 -0600

    commit

---
+++ ex8.py
@@ -0,0 +1,12 @@
+formatter = "{} {} {} {}"
+
+print(formatter.format(1, 2, 3, 4))
+print(formatter.format("one", "two", "three", "four"))
+print(formatter.format(True, False, False, True))
+print(formatter.format(formatter, formatter, formatter, formatter))
+print(formatter.format(
+    "Niggas iffy uh",
+    "Blicky got the stiffy uh",
+    "Got the blicky uh",
+    "Drum holds fifty uh",
+))
