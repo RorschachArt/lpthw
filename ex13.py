ec6d27954e0da1d23018a23025249fb6d6264f69
RorschachArt <96551787+RorschachArt@users.noreply.github.com>
2022-01-10 17:43:45 -0600

    comit

---
+++ ex13.py
@@ -0,0 +1,9 @@
+from _typeshed import FileDescriptor
+from sys import argv
+# read the WYSS section for how to ruin this 
+script, first, second, third = argv
+
+print("The script is called:", script)
+print("Your first varible is:", first)
+print("Your second varible is:", second)
+print("Your third varible is:", third)
